453ce1c8cf327dfef707051c9475970d032390e0
simon <myemail@hotmail.com>
2023-10-16 17:13:28 +0200

    added chess_piece.py

---
+++ src/chess_piece.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from piece_color import Color
+from piece_type import PieceType
+
+
+
+class chess_piece:
+
+    def __init__(self, piece_color,piece_type):
+        self.piece_type = piece_type
+        self.piece_color = piece_color
+
+    def __str__(self):
+        return f'{self.piece_color} {self.piece_type}'
+    
+    def getType(self):
+        return self.piece_type.name
+
+    def getColor(self):
+        return self.piece_color.name
+    
+    
+piece = chess_piece(Color.WHITE, PieceType.BISHOP)
+
+print(piece.getColor())
+print(piece.getType())
+print(piece.__str__())

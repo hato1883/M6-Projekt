6bbd27d5f4376ace70e4ec29b4a0129fe193aeb1
simon <myemail@hotmail.com>
2023-10-17 13:54:45 +0200

    added piece_has_moved attri & methods for get/set

--- src/chess_piece.py
+++ src/chess_piece.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Any
 from piece_color import Color
 from piece_type import PieceType
 
@@ -9,12 +10,19 @@ class chess_piece:
     def __init__(self, piece_color,piece_type):
         self.piece_type = piece_type
         self.piece_color = piece_color
+        self.piece_has_moved = False
 
     def __str__(self):
-        return f'{self.piece_color} {self.piece_type}'
+        return f'{self.piece_color} {self.piece_type} {self.piece_has_moved}'
     
     def getType(self):
         return self.piece_type.name
 
     def getColor(self):
         return self.piece_color.name
+    
+    def get_has_moved(self):
+        return self.piece_has_moved.name
+
+    def set_has_moved_true(self):
+        self.piece_has_moved = True

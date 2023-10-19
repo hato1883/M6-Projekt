55e1da223f79f6aa55d0cf2187890ad1d4f8ab82
Hampus Toft <hampus.toft.1883@student.uu.se>
2023-10-19 21:43:30 +0200

    Test for create_board()

--- tests/test_chessboard.py
+++ tests/test_chessboard.py
@@ -12,3 +12,38 @@ current = os.path.dirname(__file__)
 sys.path.append(current + "/../src")
 
 from chessboard import *
+
+# Test if create board returns a 2d list filed with None
+def test_create_board():
+    chessboard_ref = Chessboard()
+    expected_board = [
+        [None]
+        ]
+    chessboard_ref.create_board(1)
+    
+    assert len(expected_board) == len(chessboard_ref.chessboard_list)
+    for row in range(len(expected_board)):
+        # Check amount of columns in the row are equal
+        assert len(expected_board[row]) == len(chessboard_ref.chessboard_list[row])
+        for col in range(len(expected_board)):
+            assert chessboard_ref.chessboard_list[row][col] == None
+    
+    expected_board = [
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None]
+        ]
+    chessboard_ref.create_board() # default size is 8
+
+    assert len(expected_board) == len(chessboard_ref.chessboard_list)
+    for row in range(len(expected_board)):
+        # Check amount of columns in the row are equal
+        assert len(expected_board[row]) == len(chessboard_ref.chessboard_list[row])
+        for col in range(len(expected_board)):
+            assert chessboard_ref.chessboard_list[row][col] == None
+

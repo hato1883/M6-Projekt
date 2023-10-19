f1931291f0b0220a03218f1cf56b25762d64b303
Hampus Toft <hampus.toft.1883@student.uu.se>
2023-10-19 23:18:50 +0200

    test to check if piece is in danger

--- tests/test_chessboard.py
+++ tests/test_chessboard.py
@@ -109,6 +109,49 @@ def test_remove_piece():
     __is_board_equal(expected.chessboard_list, ref.chessboard_list)
 
 
+def test_in_danger():
+    b_ro = ChessPiece(Color.BLACK, PieceType.ROOK)
+    b_kn = ChessPiece(Color.BLACK, PieceType.KNIGHT)
+    b_bi = ChessPiece(Color.BLACK, PieceType.BISHOP)
+    b_qu = ChessPiece(Color.BLACK, PieceType.QUEEN)
+    b_ki = ChessPiece(Color.BLACK, PieceType.KING)
+    b_pa = ChessPiece(Color.BLACK, PieceType.PAWN)
+    # White Pieces
+    w_ro = ChessPiece(Color.WHITE, PieceType.ROOK)
+    w_kn = ChessPiece(Color.WHITE, PieceType.KNIGHT)
+    w_bi = ChessPiece(Color.WHITE, PieceType.BISHOP)
+    w_qu = ChessPiece(Color.WHITE, PieceType.QUEEN)
+    w_ki = ChessPiece(Color.WHITE, PieceType.KING)
+    w_pa = ChessPiece(Color.WHITE, PieceType.PAWN)
+
+    chessboard: list[list[ChessPiece]] = [
+        [b_ro, b_kn, b_bi, b_qu, b_ki, b_bi, b_kn, b_ro],
+        [b_pa, b_pa, b_pa, b_pa, b_pa, b_pa, b_pa, b_pa],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [w_pa, w_pa, w_pa, w_pa, w_pa, w_pa, w_pa, w_pa],
+        [w_ro, w_kn, w_bi, w_qu, w_ki, w_bi, w_kn, w_ro],
+    ]
+
+    board_refrence = Chessboard(chessboard)
+    # check
+    assert board_refrence.in_danger((0,0), Color.BLACK) == False
+    assert board_refrence.in_danger((0,0), Color.WHITE) == False
+    assert board_refrence.in_danger((0,1), Color.BLACK) == False
+    assert board_refrence.in_danger((0,1), Color.WHITE) == True
+
+    assert board_refrence.in_danger((1,1), Color.BLACK) == False
+    assert board_refrence.in_danger((3,1), Color.BLACK) == False
+    assert board_refrence.in_danger((5,1), Color.BLACK) == False
+
+    board_refrence.add_piece(w_qu, (3,3))
+    assert board_refrence.in_danger((1,1), Color.BLACK) == True
+    assert board_refrence.in_danger((3,1), Color.BLACK) == True
+    assert board_refrence.in_danger((5,1), Color.BLACK) == True
+
+
 def __is_board_equal(expected: list[list[ChessPiece]], recived: list[list[ChessPiece]]):
     assert len(expected) == len(recived)
     for row in range(len(expected)):
@@ -122,3 +165,5 @@ def __is_board_equal(expected: list[list[ChessPiece]], recived: list[list[ChessP
                 assert expected[row][col].get_type() == recived[row][col].get_type()
             else:
                 assert expected[row][col] == recived[row][col]
+
+test_in_danger()

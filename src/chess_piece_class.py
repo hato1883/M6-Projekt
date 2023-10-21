12cbc470d075ea499f3159efd318f05e1e30a6a4
Hampus Toft <hampus.toft.1883@student.uu.se>
2023-10-21 20:07:22 +0200

    added str(), representatin, == and != opperators

--- src/chess_piece_class.py
+++ src/chess_piece_class.py
@@ -12,12 +12,6 @@ class ChessPiece:
         self.piece_color = piece_color
         self.piece_has_moved = False
 
-    def __str__(self):
-        # prints □ if piece is black or ■ if piece is white
-        # Followed by First letter of piece type
-        # Followed by a dash, then F/T to represent if piece has moved.
-        return f'{"■" if self.piece_color.name is Color.WHITE.name else "□"}{str(self.piece_type.name)[0]}-{"T" if self.piece_has_moved else "F"}'
-    
     def get_type(self):
         return self.piece_type
 
@@ -29,3 +23,22 @@ class ChessPiece:
 
     def set_has_moved_true(self):
         self.piece_has_moved = True
+
+    def __str__(self):
+        # prints □ if piece is black or ■ if piece is white
+        # Followed by First letter of piece type
+        # Followed by a dash, then F/T to represent if piece has moved.
+        return f'{"■" if self.piece_color.name is Color.WHITE.name else "□"}{str(self.piece_type.name)[0]}-{"T" if self.piece_has_moved else "F"}'
+    
+    def __repr__(self) -> str:
+        return f"<ChessPiece:{self.piece_color} {self.piece_type}, {self.piece_has_moved}>"
+
+    def __eq__(self, __value: object) -> bool:
+        
+        if isinstance(__value, self.__class__):
+            return self.__dict__ == __value.__dict__
+        else:
+            return NotImplemented
+
+    def __ne__(self, __value: object) -> bool:
+        return not self.__eq__(__value)

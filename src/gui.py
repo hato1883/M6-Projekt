89169482ae3b67891fad8e8855676592f0c83a72
hahadude497 <48885873+hahadude497@users.noreply.github.com>
2023-10-24 21:36:54 +0200

    Made gui into a class

--- src/gui.py
+++ src/gui.py
@@ -1,57 +1,117 @@
+#### TODO More shit, draw the actual chesspieces, find
+#### out how to make the program relate the chessboard to the graphics
 import pygame
 import sys
+from chessboard import *
 from ui_interface import UI_Interface
-ui = UI_Interface
 
-WIDTH, HEIGHT = 600, 600
-SQUARE_SIZE = WIDTH // 8  # Divide the width into 8 squares
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
 
-ui.show_splash_screen
 
-def open_window():
-    pygame.init()
-    def draw_chessboard():
+
+class Window_Class(UI_Interface):
+    
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
+    WIDTH, HEIGHT = 600,600
+
+    SQUARE_SIZE = WIDTH // 8  # Divide the width into 8 squares
+    def __init__(self, size=(800, 600)) -> None:
+        
+        pygame.init()
+        self.size = size
+        self.screen = pygame.display.set_mode(self.size)
+        pygame.display.set_caption('Sago-Schack')
+        self.running = True
+        self.show_splash_screen
+        self.font = pygame.font.Font(None, 36)
+
+
+    def run(self):
+        #show splashscreen
+        self.running=True
+
+        self.show_splash_screen()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            #show splashscreen
+
+            #paint the background
+            self.screen.fill(((43, 42, 51)))
+
+            #call the show_chessboard function
+            self.show_chess_board()
+            pygame.display.flip()
+
+        pygame.quit()
+        sys.exit()
+
+
+    def show_splash_screen(self):
+        # Display game name, authors, date/build
+
+        # Display text in the middle of the screen
+        text = self.font.render("Sago-Shack, by: Hampus, John & Simon", True, (255, 255, 255))
+        text_rect = text.get_rect()
+        text_rect.center = (self.size[0] // 2, self.size[1] // 2)
+        self.screen.blit(text, text_rect)
+
+        #Draw the text
+        pygame.display.flip()
+        
+        #Wait 2000 ms
+        pygame.time.delay(2000)
+
+
+
+
+
+        pass
+
+    def input_game_setup_parameters():
+        # Present user with successive choices of:
+        # 1 or 2 player game (if 1, 2nd player is computer) 1 player == True
+        # Time limit per move in seconds, integer
+        # AI narration On/Off
+        # return ( two_player True/False , sec time limit integer, narration True/False)
+        pass
+
+    def show_chess_board(self):
         for row in range(8):
             for col in range(8):
-                x, y = col * SQUARE_SIZE, row * SQUARE_SIZE
+                x, y = col * self.SQUARE_SIZE, row * self.SQUARE_SIZE
                 if (row + col) % 2 == 0:
-                    pygame.draw.rect(screen, WHITE, (x, y, SQUARE_SIZE, SQUARE_SIZE))
+                    pygame.draw.rect(self.screen, self.WHITE, (x, y, self.SQUARE_SIZE, self.SQUARE_SIZE))
                 else:
-                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_SIZE, SQUARE_SIZE))
+                    pygame.draw.rect(self.screen, self.BLACK, (x, y, self.SQUARE_SIZE, self.SQUARE_SIZE))
+        pass
 
-    # Define the window size
-    window_size = (WIDTH, HEIGHT)
+    def recount_user_move():
+        # Input two 2-tupels. Origin, Dest.
+        # 2-tuple form is (row, column) were each element is integer [0,7]
+        # Recount previous move to user by text or possibly graphically
+        pass
 
-    # Create the window
-    screen = pygame.display.set_mode(window_size)
+    def input_user_move():
+        # Used to input the user's move either through algebraic notation or graphical interacivity
+        # returns two 2-tupels. Origin, Dest.
+        # 2-tuple form is (row, column) were each element is integer [0,7]
+        pass
 
-    # Set the window title
-    pygame.display.set_caption("Pygame Window with Moving Circle and chessboard")
-
-    # Game loop
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-
-        
-
-        # Draw the chessboard
-        draw_chessboard()
-        # Update the display
-        pygame.display.flip()
-
-    # Quit pygame and exit the program
-    pygame.quit()
-    sys.exit()
-
-if __name__ == "__main__":
-    open_window()
+    def input_promotion():
+        # Prompt's user to select promotion for pawn in question
+        # Multiple choice: queen, rook, bishop or knight
+        # Either by text (q, r, b or k) or graphical interaction
+        # return  integer in [0,3]. 0 == queen, 1 == r and so on.
+        pass
 
    
+if __name__ == "__main__":
+    game=Window_Class()
+    game.run()
+
+
